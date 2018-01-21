dd8b9f7979b0dc92e116a9f1d1f90bb0c19c711c
handsomegyr <handsomegyr@126.com>
2018-01-21 22:51:35 +0800

    修改

--- public/index.php
+++ public/index.php
@@ -15,10 +15,6 @@ try {
      */
     require __DIR__ . '/../config/const.php';
     
-    require __DIR__ . '/../library/MongoDate.php';
-    
-    require __DIR__ . '/../library/MongoId.php';
-    
     define('CURRENT_TIMESTAMP', time()); // 当前时间
     
     require __DIR__ . '/../vendor/autoload.php';
